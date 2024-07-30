de865cfddadd24c593b32342db2e661d04da26c9
NewAmazingPVP <gamingofpat@gmail.com>
2024-07-30 14:08:14 -0400

    added fastboard api
    added fastboard api

--- src/main/java/mod/minigameplugin/MinigamePlugin.java
+++ src/main/java/mod/minigameplugin/MinigamePlugin.java
@@ -1,17 +1,63 @@
-package mod.minigameplugin;
+package fr.mrmicky.fastboard.example;
 
+import fr.mrmicky.fastboard.FastBoard;
+import org.bukkit.ChatColor;
+import org.bukkit.Statistic;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MinigamePlugin extends JavaPlugin {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public final class MinigamePlugin extends JavaPlugin implements Listener {
+
+    private final Map<UUID, FastBoard> boards = new HashMap<>();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
 
+        getServer().getScheduler().runTaskTimer(this, () -> {
+            for (FastBoard board : this.boards.values()) {
+                updateBoard(board);
+            }
+        }, 0, 20);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        FastBoard board = new FastBoard(player);
+
+        board.updateTitle(ChatColor.RED + "FastBoard");
+
+        this.boards.put(player.getUniqueId(), board);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+
+        FastBoard board = this.boards.remove(player.getUniqueId());
+
+        if (board != null) {
+            board.delete();
+        }
+    }
+
+    private void updateBoard(FastBoard board) {
+        board.updateLines(
+                "",
+                "Players: " + getServer().getOnlinePlayers().size(),
+                "",
+                "Kills: " + board.getPlayer().getStatistic(Statistic.PLAYER_KILLS),
+                ""
+        );
     }
 }
