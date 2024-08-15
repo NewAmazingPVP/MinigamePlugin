6ab3d0ee737872fca09dad4cd8354f0892715fde
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-08-15 19:29:02 -0400

    ?????????????????

--- src/main/java/mod/minigameplugin/Commands/GiveHealthPack.java
+++ src/main/java/mod/minigameplugin/Commands/GiveHealthPack.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import static mod.minigameplugin.ZombieSurvivalGame.ZombieGameItems.HealPack.HealPackChargesManager.arrayList;
 import static mod.minigameplugin.ZombieSurvivalGame.ZombieGameItems.HealPack.HealPackChargesManager.healthPack;
 
 public class GiveHealthPack implements CommandExecutor {
@@ -13,6 +14,8 @@ public class GiveHealthPack implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         Player player = (Player) sender;
+
+        player.sendMessage(String.valueOf(arrayList.size())); 
 
         player.getInventory().addItem(healthPack(player));
 
