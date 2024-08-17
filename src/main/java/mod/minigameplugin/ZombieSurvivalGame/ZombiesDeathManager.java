d18233b93a7845c7420b8c38d2e7db741464228c
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-08-17 18:14:40 -0400

    scoreboard test

---
+++ src/main/java/mod/minigameplugin/ZombieSurvivalGame/ZombiesDeathManager.java
@@ -0,0 +1,15 @@
+package mod.minigameplugin.ZombieSurvivalGame;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZombiesDeathManager {
+
+    public static List<Player> zombiesPlayersAlive = new ArrayList<>();
+
+    public static List<Player> zombiesPlayersDead = new ArrayList<>();
+
+
+}
