1753a1f0e19f1653d2fdd481e2eda6818da7d4d4
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-08-19 18:00:56 -0400

    Update UpgradesMenu.java

--- src/main/java/mod/minigameplugin/ZombieSurvivalGame/PassiveUpgrades/UpgradesMenu.java
+++ src/main/java/mod/minigameplugin/ZombieSurvivalGame/PassiveUpgrades/UpgradesMenu.java
@@ -2,17 +2,39 @@ package mod.minigameplugin.ZombieSurvivalGame.PassiveUpgrades;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UpgradesMenu {
 
     public static void openPlayerUpgradesMenu(Player player){
 
-        Inventory playerUpgradeMenu = Bukkit.createInventory(null, 36, ChatColor.AQUA + "" + ChatColor.BOLD + "         Upgrade Menu");
+        Inventory playerUpgradeMenu = Bukkit.createInventory(null, 54, ChatColor.AQUA + "" + ChatColor.BOLD + "        Upgrade Menu");
+
+        playerUpgradeMenu.setItem(0,guiGlass_BLACK());
+
 
         player.openInventory(playerUpgradeMenu);
 
+    }
+
+
+    private static ItemStack guiGlass_BLACK(){
+
+        ItemStack guiGlass = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
+        ItemMeta meta = guiGlass.getItemMeta();
+
+        meta.setDisplayName(" ");
+
+
+        guiGlass.setItemMeta(meta);
+        return guiGlass;
     }
 
 
